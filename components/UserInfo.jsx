39b5f36644434c70884e313ec5af5fc05cd7515b
markbensonhernando <mbhernando27@gmail.com>
2023-10-09 23:57:13 +0800

    Feat: Frontend UI.

---
+++ components/UserInfo.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const UserInfo = () => {
+  return (
+    <div className='grid place-items-center h-screen'>
+        <div className='shadow-lg p-8 bg-zinc-300/10
+            flex flex-col gap-2 my-6 rounded-md'>
+            <div>
+                Name: <span className='font-bold'>Mark</span>
+            </div>
+            <div>
+                Email: <span className='font-bold'>mb@gmail.com</span>
+            </div>
+            <button className='bg-red-500 text-white font-bold
+                px-6 py-2 mt-3 rounded'>
+                Log out
+            </button>
+        </div>
+    </div>
+  )
+};
+
+export default UserInfo;
