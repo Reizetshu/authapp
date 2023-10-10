f453c24022a7cac9056364265c995b8ac67bf4e7
markbensonhernando <mbhernando27@gmail.com>
2023-10-10 09:40:45 +0800

    Feat: adding useState at RegisterForm

--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -1,6 +1,36 @@
+'use client';
+
 import Link from 'next/link';
+import { useState } from 'react';
 
 const RegisterForm = () => {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [hasError, setHasError] = useState('');
+
+    const nameHandler = (e) => {
+        setName(e.target.value);
+    }
+
+    const emailHandler = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const passwordHandler = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        if (!name || !email || !password) {
+            setHasError('All field are necessary.')
+            return
+        }
+    }
+
   return (
     <div className='grid place-items-center h-screen'>
         <div className='shadow-lg p-4 rounded-lg border-t-4 
@@ -8,18 +38,28 @@ const RegisterForm = () => {
             <h1 className='text=xl font-bold my-4'>Register</h1>
 
 
-            <form className='flex flex-col gap-3'>
-                <input type='text' placeholder='Full Name' />
-                <input type='text' placeholder='Email' />
-                <input type='password' placeholder='Password' />
+            <form onSubmit={submitHandler} className='flex flex-col gap-3'>
+                <input onChange={nameHandler} 
+                    type='text' 
+                    placeholder='Full Name' />
+                <input onChange={emailHandler} 
+                    type='text' 
+                    placeholder='Email' />
+                <input onChange={passwordHandler} 
+                    type='password' 
+                    placeholder='Password' />
                 <button className='bg-green-600 text-white
                     font-bold cursor-pointer px-6 py-2 rounded'>
                     Register
                 </button>
-                <div className='bg-red-500 text-white w-fit text-sm
+
+                { hasError && (
+                    <div className='bg-red-500 text-white w-fit text-sm
                     py-1 px-3 rounded-md mt-2'>
-                    Error Message
-                </div>
+                        {hasError}
+                    </div>
+                )}
+                
                 <Link className='text-sm mt-3 text-right' href={'/'}>
                     Already have an account? <span className='underline'>Login</span>
                 </Link>
